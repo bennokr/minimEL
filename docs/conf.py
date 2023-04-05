53a9043f11965166c0aabffdec766fefed69cdf8
Benno Kruit <bennokr@gmail.com>
2023-04-05 12:58:47 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,44 +3,63 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../minimel'))
+
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'minimel'
-copyright = '2022, Benno Kruit'
+copyright = '2023, Benno Kruit'
 author = 'Benno Kruit'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx_autodoc_typehints']
+extensions = [
+    'sphinxcontrib.apidoc',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx_rtd_theme',
+    'myst_parser',
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
-
-
-
+# Apidoc and autodoc config
 apidoc_module_dir = '../minimel'
 apidoc_toc_file = False
 apidoc_module_first = True
 apidoc_separate_modules = True
 apidoc_extra_args = ['-F']
-autodoc_default_flags = ['members', 'titlesonly', 'show-inheritance']
+autodoc_default_flags = ['members']
+autodoc_member_order = 'bysource'
+autosummary_generate = True
 
-# autosummary_generate = True
-
+# Napoleon config
 napoleon_google_docstring = True
 napoleon_use_param = True
 napoleon_use_keyword = True
 napoleon_use_rtype = True
+
+# Intersphinx config
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+}
+# intersphinx_disabled_reftypes = ["*"]
+
+
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ['_static']
