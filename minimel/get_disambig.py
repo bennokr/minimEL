8d0a7e1247e3b5c911751d474007c4699e04253f
Benno Kruit <bennokr@gmail.com>
2023-05-23 10:21:07 +0000

    disambig template

--- minimel/get_disambig.py
+++ minimel/get_disambig.py
@@ -18,9 +18,15 @@ except ImportError:
 from .scale import fileparts
 
 
-def get_list_links(page):
+def get_list_links(page, disambig_template=None):
     code = mwparserfromhell.parse(page)
     nodes = code.filter(matches=(lambda x: str(x).strip()), recursive=True)
+
+    if disambig_template:
+        ts = code.ifilter_templates()
+        if not any(t.name.lower() == disambig_template.lower() for t in ts):
+            return
+
     for i, node in enumerate(nodes[1:]):
         if getattr(nodes[i], "tag", None) == "li":
             if type(node) == mwparserfromhell.nodes.Wikilink:
@@ -33,13 +39,16 @@ def get_list_links(page):
                         yield str(link.title)
 
 
-def get_disambig_links(lines, dawgfile, disambig_ents_file):
+def get_disambig_links(
+    lines, dawgfile, wikidata_disambigfile=None, disambig_template=None
+):
     index = dawg.IntDAWG()
     index.load(dawgfile)
 
-    wikidata_disambigfile = set(
-        int(q.replace("Q", "")) for q in open(disambig_ents_file).readlines()
-    )
+    if wikidata_disambigfile:
+        disambig_ents = set(
+            int(q.replace("Q", "")) for q in open(wikidata_disambigfile).readlines()
+        )
     output = []
     for line in lines:
         line = line.replace("\\n", "\n")
@@ -47,9 +56,9 @@ def get_disambig_links(lines, dawgfile, disambig_ents_file):
         title = elem.find("./title").text.replace(" ", "_")
         text = elem.find("./revision/text").text
 
-        if index.get(title) in wikidata_disambigfile:
+        if (not wikidata_disambigfile) or (index.get(title) in disambig_ents):
             links = set()
-            for link in get_list_links(text):
+            for link in get_list_links(text, disambig_template=disambig_template):
                 link = link.replace(" ", "_")
                 if link in index:
                     links.add(index[link])
@@ -61,8 +70,9 @@ def get_disambig_links(lines, dawgfile, disambig_ents_file):
 def get_disambig(
     wikidump: pathlib.Path,
     dawgfile: pathlib.Path,
-    wikidata_disambigfile: pathlib.Path,
+    wikidata_disambigfile: pathlib.Path = None,
     *,
+    disambig_template: str = None,
     nparts: int = 1000,
 ):
     """
@@ -77,10 +87,16 @@ def get_disambig(
 
     Keyword Arguments:
         nparts: Number of chunks to read
+        disambig-template: If no wikidata_disambigfile is provided, consider disambiguation pages that contain a template with this name
     """
 
     import dask.bag as db
     from .scale import progress, get_client
+
+    if disambig_template:
+        logging.info(
+            f"Using disambiguation template {disambig_template}, not {wikidata_disambigfile}"
+        )
 
     with get_client():
         bag = db.from_sequence(range(nparts), npartitions=nparts).map_partitions(
@@ -88,7 +104,12 @@ def get_disambig(
         )
 
         links = bag.map_partitions(
-            lambda b: get_disambig_links(b, str(dawgfile), str(wikidata_disambigfile))
+            lambda b: get_disambig_links(
+                b,
+                str(dawgfile),
+                str(wikidata_disambigfile or ""),
+                disambig_template=disambig_template,
+            )
         )
         logging.info(f"Extracting disambiguation links...")
         if logging.root.level < 30:
